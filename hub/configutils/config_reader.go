maduhu/gpdb

gp_upgrade: convert master
Given user has set up an old and new cluster, they should be able to
upgrade the master nodes.

- Use cluster config files to determine master data directory
- Add tests for convert master in the hub side.
  "Mock" out exec.Command to give specific outputs and exit status.

- Save pg_upgrade log to pg_upgrade_master.log

Signed-off-by: Nadeem Ghani <nghani@pivotal.io>

gpMgmt/go-utils/src/gp_upgrade/hub/configutils/config_reader.go
@@ -75,3 +75,26 @@ func (reader Reader) GetHostnames() []string {
 func (reader Reader) GetSegmentConfiguration() SegmentConfiguration {
 	return reader.config
 }
+
+func (reader Reader) GetMasterDataDir() string {
+	config := reader.GetSegmentConfiguration()
+	for i := 0; i < len(config); i++ {
+		segment := config[i]
+		if segment.Content == -1 {
+			return segment.Fselocation
+		}
+	}
+	return ""
+}
+
+func (reader Reader) GetMaster() *Segment {
+	var nilSegment *Segment
+	config := reader.GetSegmentConfiguration()
+	for i := 0; i < len(config); i++ {
+		segment := config[i]
+		if segment.Content == -1 {
+			return &segment
+		}
+	}
+	return nilSegment
+}

skahler-pivotal/gpupgrade

hub/configutils/config_reader.go
@@ -75,3 +75,26 @@ func (reader Reader) GetHostnames() []string {
 func (reader Reader) GetSegmentConfiguration() SegmentConfiguration {
 	return reader.config
 }
+
+func (reader Reader) GetMasterDataDir() string {
+	config := reader.GetSegmentConfiguration()
+	for i := 0; i < len(config); i++ {
+		segment := config[i]
+		if segment.Content == -1 {
+			return segment.Datadir
+		}
+	}
+	return ""
+}
+
+func (reader Reader) GetMaster() *Segment {
+	var nilSegment *Segment
+	config := reader.GetSegmentConfiguration()
+	for i := 0; i < len(config); i++ {
+		segment := config[i]
+		if segment.Content == -1 {
+			return &segment
+		}
+	}
+	return nilSegment
+}